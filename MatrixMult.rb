28798669c983aac7eed730fa821f4a48f86f8004
gheard17 <gheard@u.rochester.edu>
2016-04-02 18:12:33 -0400

    bugs in mult

--- MatrixMult.rb
+++ MatrixMult.rb
@@ -2,8 +2,8 @@ require 'matrix'
 
 inp = $stdin.read
 arr = inp.split "\n"
-x = arr[0..arr.size/2-1]
+x = arr[0..arr.size/2-5]
 y = arr[arr.size/2.. arr.size] 
 m = Matrix.rows x.map{|a| a.split(" ").map{|a| a.to_i}} 
-n = Matrix.rows y.map{|a| a.split(" ").map{|a| a.to_i}}
+n = Matrix.rows y.map{|a| a.split(" \n").map{|a| a.to_i}}
 puts (m * n)
