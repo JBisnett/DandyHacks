a87c0eb039ee2bc6b274b86bdbb5b8d64b32d2df
gheard17 <gheard@u.rochester.edu>
2016-04-02 18:13:24 -0400

    bugs in tran

--- MatrixTran.rb
+++ MatrixTran.rb
@@ -1,7 +1,7 @@
 require 'matrix'
 
 inp = $stdin.read
-arr = inp.split "\n"
+arr = inp..split "\n"
 x = arr[0..arr.size]
 m = Matrix.rows x.map{|a| a.split(" ").map{|a| a.to_i}} 
-puts m.transpose
+puts m.transposes
